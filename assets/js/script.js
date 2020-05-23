e44e017d630b1a43e2064c43f8e637ee16c522a8
Snouzy <mathiasnouzy@gmail.com>
2020-05-24 00:12:35 +0200

    adding js to the mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const hmbgr = document.querySelector('.hamburger');
+hmbgr.addEventListener('click', function(){
+    document.querySelector('body').classList.toggle('blue');
+    document.querySelector('.nav-links').classList.toggle('appear');
+    const lines = document.querySelectorAll('.line');
+    [...lines].forEach(el => el.classList.toggle('white'));
+    document.querySelector('.logo-text').classList.toggle('text-white');
+})
