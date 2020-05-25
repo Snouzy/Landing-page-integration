82e26b6c6686fe7c619ac0ecae2aeb4b5a396877
Snouzy <mathiasnouzy@gmail.com>
2020-05-25 23:39:00 +0200

    Adding the social icons on mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const lines = document.querySelectorAll('.line');
 hmbgr.addEventListener('click', function(){
     toggleClass('.voile', 'appear');
     toggleClass('.nav-links', 'appear');
+    toggleClass('.nav-links-container', 'appear');
     toggleClass('.logo-text', 'text-white');
     toggleClass('.logo-icon', 'logo-icon-opened');
 
@@ -13,6 +14,12 @@ hmbgr.addEventListener('click', function(){
     });
 })
 
+
+/**
+ * Séléctionne un élément afin de lui toggle une classe
+ * @param {HTMLElement} elementToToggle l'élément HTML à sélectionner
+ * @param {string} classToToggle La classe à toggle à l'élment
+ */
 const toggleClass = (elementToToggle, classToToggle) => {
     document.querySelector(elementToToggle).classList.toggle(classToToggle);
 }
